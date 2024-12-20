a66fc7d33bab95f626b0f45e5d6cb7cdac6d67a3
ribana-b <ribana-b@student.42malaga.com>
2024-12-20 20:06:13 +0100

    Add more tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -17,8 +17,67 @@ void	myTest(void)
 	st.beRepaired(10);
 }
 
+void	myTest2(void)
+{
+	ScavTrap	st;
+
+	st.guardGate();
+	st.guardGate();
+	st.takeDamage(20);
+	for (int i = 0; i < 100 && st.getHitPoints() > 0 && st.getEnergyPoints() > 0; ++i)
+	{
+		st.attack("foo");
+		st.takeDamage(5);
+		st.beRepaired(4);
+	}
+	st.guardGate();
+	st.attack("foo");
+	st.beRepaired(10);
+}
+
+void	myTest3(void)
+{
+	ScavTrap	st("_patrick");
+
+	st.guardGate();
+	st.guardGate();
+	st.takeDamage(20);
+	for (int i = 0; i < 100 && st.getHitPoints() > 0 && st.getEnergyPoints() > 0; ++i)
+	{
+		st.attack("foo");
+		st.takeDamage(i);
+		st.beRepaired(i);
+	}
+	st.guardGate();
+	st.attack("foo");
+	st.beRepaired(10);
+}
+
+void	myTest4(void)
+{
+	ScavTrap	st;
+	ScavTrap	st2("_peter");
+
+	st.guardGate();
+	st = st2;
+	st.guardGate();
+	st.takeDamage(20);
+	for (int i = 0; i < 100 && st.getHitPoints() > 0 && st.getEnergyPoints() > 0; ++i)
+	{
+		st.attack("foo");
+		st.takeDamage(i);
+		st.beRepaired(i);
+	}
+	st.guardGate();
+	st.attack("foo");
+	st.beRepaired(10);
+}
+
 int	main()
 {
 	myTest();
+	myTest2();
+	myTest3();
+	myTest4();
 	return (0);
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -15,8 +15,61 @@ void	myTest(void)
 	ft.beRepaired(10);
 }
 
+void	myTest2(void)
+{
+	FragTrap	ft("_mytest2");
+
+	ft.highFivesGuys();
+	for (int i = 0; i < 10 && ft.getHitPoints() > 0 && ft.getEnergyPoints() > 0; ++i)
+	{
+		ft.attack("lol");
+		ft.takeDamage(11);
+		ft.beRepaired(10);
+	}
+	ft.highFivesGuys();
+	ft.attack("lol");
+	ft.beRepaired(10);
+}
+
+void	myTest3(void)
+{
+	FragTrap	ft("_mytest3");
+
+	ft.highFivesGuys();
+	for (int i = 0; i < 100 && ft.getHitPoints() > 0 && ft.getEnergyPoints() > 0; ++i)
+	{
+		ft.attack("lol");
+		ft.takeDamage(11);
+		ft.beRepaired(10);
+	}
+	ft.highFivesGuys();
+	ft.attack("lol");
+	ft.beRepaired(10);
+}
+
+void	myTest4(void)
+{
+	FragTrap	ft;
+	FragTrap	ft2("_mytest4");
+
+	ft = ft2;
+	ft.highFivesGuys();
+	for (int i = 0; i < 100 && ft.getHitPoints() > 0 && ft.getEnergyPoints() > 0; ++i)
+	{
+		ft.attack("lol");
+		ft.takeDamage(11);
+		ft.beRepaired(10);
+	}
+	ft.highFivesGuys();
+	ft.attack("lol");
+	ft.beRepaired(10);
+}
+
 int	main()
 {
 	myTest();
+	myTest2();
+	myTest3();
+	myTest4();
 	return (0);
 }
